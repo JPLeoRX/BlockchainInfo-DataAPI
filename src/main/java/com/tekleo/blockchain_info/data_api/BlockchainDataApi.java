7992c244c1b88979b287e969e1227d59cad4d84d
leo <leo.ertuna@gmail.com>
2018-01-21 01:29:33 +0200

    Declared core api interface

--- src/main/java/com/tekleo/blockchain_info/data_api/BlockchainDataApi.java
+++ src/main/java/com/tekleo/blockchain_info/data_api/BlockchainDataApi.java
@@ -1,4 +1,31 @@
 package com.tekleo.blockchain_info.data_api;
 
-public class BlockchainDataApi {
+import com.tekleo.blockchain_info.data_api.models.*;
+
+import java.util.List;
+
+public interface BlockchainDataApi {
+    Block getSingleBlock(String blockHash);
+
+    Transaction getSingleTransactions(String transactionHash);
+
+    ChartData geChartData(String chartType);
+
+    BlockHeight getBlockHeight(String blockHeight);
+
+    Address getSingleAddress(String address, int limit, int offset);
+
+    AddressMultiple getMultiAddress(List<String> addresses, int n, int offset);
+
+    OutputsUnspent getUnspentOutputs(List<String> addresses, int limit, int confirmations);
+
+    Balance getBalance(List<String> addresses);
+
+    BlockLatest getLatestBlock();
+
+    TransactionsUnconfirmed getUnconfirmedTransactions();
+
+    Blocks getBlocksForTime(String timeInMilliSeconds);
+
+    Blocks getBlocksForPool(String poolName);
 }
